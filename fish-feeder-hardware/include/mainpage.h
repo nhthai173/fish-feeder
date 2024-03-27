6677eafaadc6071b63904bb4f6ff39c648101563
Thái <62926491+nhthai173@users.noreply.github.com>
2024-03-27 20:10:13 +0700

    Update main code

--- fish-feeder-hardware/include/mainpage.h
+++ fish-feeder-hardware/include/mainpage.h
@@ -1,63 +1,74 @@
 const char* MAINPAGE = R"html(
-<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css"><title>Fish Feeder</title><style>body{background:rgba(0 0 0 / 3%)}.main-view{margin-top:50px;margin-bottom:50px;width:100%;max-width:600px}.loading{position:fixed;top:0;left:0;width:100%;height:100%;display:flex;justify-content:center;align-items:center;user-select:none;z-index:1000}section{position:relative}.back-to-home{position:absolute;top:0;left:20px;cursor:pointer}.back-to-home::before{font-family:'Font Awesome 5 Pro';content:"\f177";font-weight:100;font-size:2.2em;color:var(--bs-body-color)}.loading .message{position:absolute;left:50%;bottom:50px;transform:translateX(-50%);padding:20px;border-radius:200px;font-size:1.2em;color:var(--bs-white);background:rgba(0 0 0 / 60%)}.card{margin-bottom:40px;border-width:0;border-radius:12px;box-shadow:0 0 20px 0 rgba(0,0,0,.05);padding:15px;display:flex;flex-direction:row}.card-logo-container{width:100px;height:100%;display:flex;justify-content:center;align-items:center;padding:20px 0 20px 0}.card-logo{width:65px;height:65px;border-radius:50%;display:flex;justify-content:center;align-items:center;color:var(--bs-white);background:var(--bs-primary)}.card-content{display:flex;align-items:center;padding-top:10px;margin-left:15px}.modal-header{border-width:0;padding:20px}.disabled{pointer-events:none!important;opacity:.5}.btn-p{width:100%;border-radius:200px}.btn-p-sm{max-width:150px}.btn-add{position:fixed;bottom:60px;right:60px;width:60px;height:60px;border-radius:50%;background:var(--bs-primary);box-shadow:0 0 20px 0 rgba(0,0,0,.1);cursor:pointer}.btn-add::before{font-family:'Font Awesome 5 Pro';content:"\f067";font-weight:100;font-size:2.2em;color:var(--bs-white);position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)}.expand-icon::before{font-family:'Font Awesome 5 Pro';content:"\f054";font-weight:450;font-size:1.1em;color:rgba(var(--bs-secondary-rgb),.5)}.timeline{position:relative;width:100%;height:40px;padding-top:20px;display:flex}.timeline .time::before{content:'';position:absolute;top:10px;left:95px;width:5px;height:5px;border-radius:50%;background-color:rgba(0 0 0 / 20%)}.timeline:not(:last-child) .time::after{content:'';position:absolute;top:10px;left:96.5px;width:2px;height:calc(100% + 16px);background-color:rgba(0 0 0 / 20%)}.timeline .time{position:absolute;top:0}.timeline .activity{position:absolute;top:0;left:110px}@media screen and (max-width:400px){.loading .message{padding:10px;font-size:.9em;bottom:30px}.card{padding:0}.btn-add{bottom:20px;right:20px}.back-to-home{left:15px;top:-6px;font-size:.8em}}</style></head><body><div class="loading d-none"><div class="message">Đã đặt lượng thức ăn: 2g</div></div><div class="container-fluid"><div class="row justify-content-center"><div class="col main-view placeholder-glow"><section class="fade show" id="home"><div class="mb-3 placeholder" style="width:200px;height:200px"></div><h2 class="mb-5 mb-md-4 fs-1 fs-md-2">Máy cho cá ăn</h2><div class="card d-flex" id="feed-card"><div class="card-logo-container"><div class="card-logo"><i class="fa-2x fas fa-braille"></i></div></div><div class="card-content w-100"><div class="w-100 ms-3 ms-md-1"><h4 class="mb-1" id="feed-text">Cho ăn</h4><span class="text-muted">Thức ăn:<span class="text-success">còn</span></span></div></div></div><div class="card" id="amount-card"><div class="card-body"><div class="card-title d-flex"><h4>Lượng thức ăn</h4><span class="ms-2 me-2">|</span><span class="text-muted" id="amount-text"></span></div><input type="range" class="form-range" id="amount" step="1" min="1" max="20" value="2"></div></div><div class="card d-flex" section-view="schedule" id="schedule-card"><div class="card-logo-container"><div class="card-logo"><i class="mt-md-1 fa-3x fal fa-clock" style="margin-top:0"></i></div></div><div class="card-content w-100"><div class="w-100 ms-3 ms-md-1"><h4>Hẹn giờ cho ăn</h4></div><span class="expand-icon me-3"></span></div></div><div class="card" section-view="history" id="history-card"><div class="card-body pb-0"><div class="card-title d-flex justify-content-between mb-3"><h4>Lịch sử cho ăn</h4><span class="expand-icon"></span></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div></div></div></section><section class="d-none fade show" id="schedule"><div class="modal fade" id="add-schedule-modal" tabindex="-1" aria-hidden="true"><div class="modal-dialog modal-fullscreen-sm-down"><div class="modal-content px-2"><div class="modal-header"><a class="btn-close-modal text-black" data-bs-dismiss="modal"><i class="fa-2x fal fa-times text-muted"></i></a></div><div class="modal-body pt-0"><h2 class="mb-5">Thêm hẹn giờ mới</h2><div class="fs-4"><div class="d-flex justify-content-between align-items-center mb-4"><label for="schedule-time" class="form-label w-100 fw-bold">Thời gian</label><input class="form-control fs-4" style="width:150px" type="time" id="schedule-time"></div><div class="mb-4"><label for="schedule-amount" class="form-label mb-1 fw-bold">Lượng thức ăn</label><input type="range" class="form-range" id="schedule-amount" step="1" min="1" max="20" value="2"></div><div class="d-flex justify-content-between align-items-center mb-4"><span class="fs-4 fw-bold">Lặp lại</span><label for="schedule-repeat" class="form-label">Chọn</label><select multiple="multiple" class="visually-hidden" style="z-index:-1" id="schedule-repeat"><option value="0">Thứ 2</option><option value="1">Thứ 3</option><option value="2">Thứ 4</option><option value="3">Thứ 5</option><option value="4">Thứ 6</option><option value="5">Thứ 7</option><option value="6">Chủ nhật</option></select></div></div></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button><button type="button" class="btn btn-primary">Lưu</button></div></div></div></div><div class="back-to-home"></div><div class="d-flex justify-content-center mb-5"><h2>Hẹn giờ</h2></div><div class="btn-add"></div><div class="card"><div class="card-body d-flex"><div class="d-flex flex-column w-100"><span class="fs-2">17:30</span><span class="text-muted">2g | Hằng ngày</span></div><button class="btn btn-dark">Edit</button></div></div></section><section class="d-none fade show" id="history"><div class="back-to-home"></div><div class="d-flex justify-content-center"><h2>Lịch sử cho ăn</h2></div></section></div></div></div><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script><script>function showSection(section) {
-    $('section').removeClass('show')
+<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"><link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css"><title>Fish Feeder</title><style>body{background:rgba(0 0 0 / 3%)}.main-view{margin-top:50px;margin-bottom:50px;width:100%;max-width:600px}.loading{position:fixed;top:0;left:0;width:100%;height:100%;display:flex;justify-content:center;align-items:center;user-select:none;z-index:1000}section{position:relative}.back-to-home{position:absolute;top:0;left:20px;cursor:pointer}.back-to-home::before{font-family:'Font Awesome 5 Pro';content:"\f177";font-weight:100;font-size:2.2em;color:var(--bs-body-color)}.loading .message{position:absolute;left:50%;bottom:50px;transform:translateX(-50%);padding:20px;border-radius:200px;font-size:1.2em;color:var(--bs-white);background:rgba(0 0 0 / 60%)}.card{margin-bottom:40px;border-width:0;border-radius:12px;box-shadow:0 0 20px 0 rgba(0,0,0,.05);padding:15px;display:flex;flex-direction:row}.card-logo-container{width:100px;height:100%;display:flex;justify-content:center;align-items:center;padding:20px 0 20px 0}.card-logo{width:65px;height:65px;border-radius:50%;display:flex;justify-content:center;align-items:center;color:var(--bs-white);background:var(--bs-primary)}.card-content{display:flex;align-items:center;padding-top:10px;margin-left:15px}.form-check-lg{width:60px!important;height:30px}.modal-header{border-width:0;padding:20px}.disabled{pointer-events:none!important;opacity:.5}.btn-p{width:100%;border-radius:200px}.btn-p-sm{max-width:150px}.btn-add{position:fixed;bottom:60px;right:60px;width:60px;height:60px;border-radius:50%;background:var(--bs-primary);box-shadow:0 0 20px 0 rgba(0,0,0,.1);cursor:pointer}.btn-add::before{font-family:'Font Awesome 5 Pro';content:"\f067";font-weight:100;font-size:2.2em;color:var(--bs-white);position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)}.expand-icon::before{font-family:'Font Awesome 5 Pro';content:"\f054";font-weight:450;font-size:1.1em;color:rgba(var(--bs-secondary-rgb),.5)}.timeline{position:relative;width:100%;height:40px;padding-top:20px;display:flex}.timeline .time::before{content:'';position:absolute;top:10px;left:95px;width:5px;height:5px;border-radius:50%;background-color:rgba(0 0 0 / 20%)}.timeline:not(:last-child) .time::after{content:'';position:absolute;top:10px;left:96.5px;width:2px;height:calc(100% + 16px);background-color:rgba(0 0 0 / 20%)}.timeline .time{position:absolute;top:0}.timeline .activity{position:absolute;top:0;left:110px}@media screen and (max-width:500px){.loading .message{padding:10px;font-size:.9em;bottom:30px}.card{padding:0}.btn-add{bottom:25px;right:25px}.back-to-home{left:15px;top:-6px;font-size:.8em}}</style></head><body><div class="loading d-none"><div class="message">Đã đặt lượng thức ăn: 2g</div></div><div class="container-fluid"><div class="row justify-content-center"><div class="col main-view placeholder-glow"><section class="fade show" id="home"><div class="mb-3 placeholder" style="width:200px;height:200px"></div><h2 class="mb-5 mb-md-4 fs-1 fs-md-2">Máy cho cá ăn</h2><div class="card d-flex" id="feed-card"><div class="card-logo-container"><div class="card-logo"><i class="fa-2x fas fa-braille"></i></div></div><div class="card-content w-100"><div class="w-100 ms-3 ms-md-1"><h4 class="mb-1" id="feed-text">Cho ăn</h4><span class="text-muted">Thức ăn:<span class="text-success">còn</span></span></div></div></div><div class="card" id="amount-card"><div class="card-body"><div class="card-title d-flex"><h4>Lượng thức ăn</h4><span class="ms-2 me-2">|</span><span class="text-muted" id="amount-text"></span></div><input type="range" class="form-range" id="amount" step="1" min="1" max="20" value="2"></div></div><div class="card d-flex" section-view="schedule" id="schedule-card"><div class="card-logo-container"><div class="card-logo"><i class="mt-md-1 fa-3x fal fa-clock" style="margin-top:0"></i></div></div><div class="card-content w-100"><div class="w-100 ms-3 ms-md-1"><h4>Hẹn giờ cho ăn</h4></div><span class="expand-icon me-3"></span></div></div><div class="card" section-view="history" id="history-card"><div class="card-body pb-0"><div class="card-title d-flex justify-content-between mb-3"><h4>Lịch sử cho ăn</h4><span class="expand-icon"></span></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div><div class="timeline"><div class="time"><span class="text-muted">17/03</span>19:30</div><div class="activity">5g</div></div></div></div></section><section class="d-none fade show" id="schedule"><div class="modal fade" id="add-schedule-modal" tabindex="-1" aria-hidden="true"><div class="modal-dialog modal-fullscreen-sm-down"><div class="modal-content px-2"><div class="modal-header"><a class="btn-close-modal text-black" data-bs-dismiss="modal"><i class="fa-2x fal fa-times text-muted"></i></a></div><div class="modal-body pt-0"><h2 class="mb-5">Thêm hẹn giờ mới</h2><div class="fs-4"><div class="d-flex justify-content-between align-items-center mb-4"><label for="schedule-time" class="form-label w-100 fw-bold">Thời gian</label><input class="form-control fs-4" style="width:150px" type="time" id="schedule-time"></div><div class="mb-4"><label for="schedule-amount" class="form-label mb-1 fw-bold">Lượng thức ăn</label><input type="range" class="form-range" id="schedule-amount" step="1" min="1" max="20" value="2"></div><div class="d-flex justify-content-between align-items-center mb-4"><span class="fs-4 fw-bold">Lặp lại</span><label for="schedule-repeat" class="form-label">Chọn</label><select multiple="multiple" class="visually-hidden" style="z-index:-1" id="schedule-repeat"><option value="0">Thứ 2</option><option value="1">Thứ 3</option><option value="2">Thứ 4</option><option value="3">Thứ 5</option><option value="4">Thứ 6</option><option value="5">Thứ 7</option><option value="6">Chủ nhật</option></select></div></div></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button><button type="button" class="btn btn-primary">Lưu</button></div></div></div></div><div class="back-to-home"></div><div class="d-flex justify-content-center mb-5"><h2>Hẹn giờ</h2></div><div class="btn-add"></div><div class="card"><div class="card-body d-flex align-items-center"><div class="d-flex flex-column w-100"><span class="fs-2">17:30</span><span class="text-muted">2g | Hằng ngày</span></div><div class="form-check form-switch"><input class="form-check-input form-check-lg" type="checkbox" role="switch" checked="checked"></div></div></div></section><section class="d-none fade show" id="history"><div class="back-to-home"></div><div class="d-flex justify-content-center"><h2>Lịch sử cho ăn</h2></div></section></div></div></div><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script><script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script><script>var wsLastCmd = '';
+var ws = new WebSocket(`ws://${window.location.host}:81/`);
+ws.addEventListener("message", (e) => {
+    console.log(e.data);
+    if (e.data == 'FEEDED') {
+        $('#feed-card').removeClass('disabled');
+        $('#feed-text').text('Cho ăn');
+        $('.loading').addClass('d-none');
+    }
+});</script><script>function showSection(section) {
+    $('section').removeClass('show');
     setTimeout(() => {
-        $('section').addClass('d-none')
-        $(`section#${section}`).removeClass('d-none')
+        $('section').addClass('d-none');
+        $(`section#${section}`).removeClass('d-none');
     }, 200);
     setTimeout(() => {
-        $(`section#${section}`).addClass('show')
+        $(`section#${section}`).addClass('show');
     }, 300);
 }
 
 // Section view
 $('[section-view]').click(function () {
-    showSection($(this).attr('section-view'))
+    let id = $(this).attr('section-view');
+    showSection($(this).attr('section-view'));
+    if (id == 'history')
+        ws.send('#LOGS');
+    if (id == 'schedule')
+        ws.send('#TASKS');
 })
 
 // Back to home
 $('.back-to-home').click(() => {
-    showSection('home')
+    showSection('home');
 })
 
 // Amount
 $('#amount').on('input', function () {
-    $('#amount-text').text(`${$(this).val()}g`)
+    $('#amount-text').text(`${$(this).val()}g`);
 })
 $('#amount').change(() => {
-    $('.loading .message').text(`Đã đặt lượng thức ăn: ${$('#amount').val()}g`)
-    $('.loading').removeClass('d-none')
-    $('#amount-card').addClass('disabled')
+    $('.loading .message').text(`Đã đặt lượng thức ăn: ${$('#amount').val()}g`);
+    $('.loading').removeClass('d-none');
+    $('#amount-card').addClass('disabled');
     setTimeout(() => {
-        $('.loading').addClass('d-none')
-        $('#amount-card').removeClass('disabled')
+        $('.loading').addClass('d-none');
+        $('#amount-card').removeClass('disabled');
     }, 3000);
 })
-$('#amount').trigger('input')
+$('#amount').trigger('input');
 
 
 // Feed
 $('#feed-card').click(() => {
-    $('#feed-text').text('Đang cho ăn...')
-    $('#feed-card').addClass('disabled')
-    $('.loading').removeClass('d-none')
-    $('.loading .message').text(`Đang cho ăn ${$('#amount').val()}g`)
-    setTimeout(() => {
-        $('#feed-card').removeClass('disabled');
-        $('#feed-text').text('Cho ăn');
-        $('.loading').addClass('d-none');
-    }, 2000);
+    let a = $('#amount').val();
+    ws.send(`#FEED ${a}`);
+    $('#feed-text').text('Đang cho ăn...');
+    $('#feed-card').addClass('disabled');
+    $('.loading').removeClass('d-none');
+    $('.loading .message').text(`Đang cho ăn ${a}g`);
 })
 
 // Add schedule modal
 $('.btn-add').click(() => {
-    $('#add-schedule-modal').modal('show')
+    $('#add-schedule-modal').modal('show');
 })
 
 
 // Add Schedule
 $('#schedule-amount').on('input', function () {
-    const v = $(this).val()
+    const v = $(this).val();
     const $l = $('#schedule-amount').parent().find('label')
     if (v)
         $l.text(`Lượng thức ăn: ${v}g`)
--- fish-feeder-hardware/src/main.cpp
+++ fish-feeder-hardware/src/main.cpp
@@ -3,15 +3,50 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <SimpleTimer.h>
+
+#include <WebSocketsServer.h>
+
+#include "FeedLog.h"
+#include "Scheduler.h"
+#include "Feeder.h"
 
 #include "mainpage.h"
 
+
+#define SERVO_PIN D1
+#define SENSOR_PIN D2
+#define BUTTON_PIN D0
+
+
+WiFiUDP ntpUDP;
+NTPClient timeClient = NTPClient(ntpUDP, "pool.ntp.org", 7 * 3600);
 ESP8266WebServer server(80);
+
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+SimpleTimer timer;
+FeedLog feedLog(&timeClient);
+Scheduler scheduler(&timeClient);
+Feeder feeder(SERVO_PIN);
+
+void _webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length);
+void mainLoop();
+void feed(uint8_t amount, std::function<void()> callback);
+
+bool buttonState;
+bool lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup()
 {
 
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(SENSOR_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   Serial.begin(115200);
   while (!Serial)
@@ -28,7 +63,11 @@ void setup()
 
   /* ====== Wait for wifi connection ====== */
   WiFiManager wifiManager;
-  wifiManager.autoConnect();
+
+  // // Reset wifimanager
+  // wifiManager.resetSettings();
+  String hotspotName = "Fish Feeder_" + String(ESP.getChipId(), HEX);
+  wifiManager.autoConnect(hotspotName.c_str());
 
   // Print local IP
   Serial.println(WiFi.localIP());
@@ -38,18 +77,89 @@ void setup()
   server.on("/", []()
             { server.send(200, "text/html", MAINPAGE); });
 
+  server.on("/logs", []()
+            { server.send(200, "text/plain", feedLog.gets()); });
+
+  server.on("/schedules", []()
+            { server.send(200, "text/plain", scheduler.getString()); });
+
+  server.on("/addtask", []()
+            {
+    schedule_task_t task;
+    task.id = rand() % 100;
+    task.time.hour = 21;
+    task.time.minute = 0;
+    task.repeat.monday = true;
+    task.repeat.tuesday = true;
+    task.repeat.wednesday = true;
+    task.repeat.thursday = true;
+    task.repeat.friday = true;
+    task.repeat.saturday = true;
+    task.repeat.sunday = true;
+    task.amount = 3;
+    task.enabled = true;
+    task.executed = false;
+    bool result = scheduler.addTask(task);
+
+    if (!result)
+    {
+      server.send(500, "text/plain", "Failed to add task");
+      return;
+    }
+
+    timer.setTimeout(2000, [](){
+      scheduler.printToSerial(Serial);
+    });
+
+    server.send(200, "text/plain", "Task added " + String(task.id)); });
+
   server.onNotFound([]()
                     { server.send(404, "text/plain", "404: Not found"); });
 
-  /* ====== Start web server ====== */
+  /* ====== Start services ====== */
+
   server.begin();
+  timeClient.begin();
+
+  webSocket.begin();
+  webSocket.onEvent(_webSocketEvent);
+
+  timer.setTimeout(1000, []()
+                   {
+    Serial.println("======== Feed log ========");
+    Serial.println(feedLog.gets().c_str());
+    Serial.println();
+    Serial.println();
+    Serial.println("======== Scheduler ========");
+    scheduler.printToSerial(Serial); });
+
+  timer.setInterval(300, mainLoop);
 }
-
-
 
 void loop()
 {
+  timer.run();
   server.handleClient();
+  webSocket.loop();
+  feeder.run();
+
+  int reading = digitalRead(BUTTON_PIN);
+  if (reading != lastButtonState)
+  {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == LOW) {
+        feed(30, [](){
+          Serial.println("Feeded");
+        });
+      }
+    }
+  }
+
+
   if (WiFi.status() == WL_CONNECTED)
   {
     digitalWrite(LED_BUILTIN, LOW);
@@ -58,4 +168,66 @@ void loop()
   {
     digitalWrite(LED_BUILTIN, HIGH);
   }
+}
+
+void _webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED:
+  {
+    IPAddress ip = webSocket.remoteIP(num);
+    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+    // send message to client
+    webSocket.sendTXT(num, "Connected");
+  }
+  break;
+  case WStype_TEXT:
+    Serial.printf("[%u] get Text: %s\n", num, payload);
+    String message = String((char *)(payload));
+    if (message.startsWith("#FEED"))
+    {
+      // #FEED <n>
+      webSocket.broadcastTXT("FEEDING");
+      feed(
+          message.substring(6).toInt(),
+          []()
+          {
+            webSocket.broadcastTXT("FEEDED");
+          });
+    }
+    if (message.startsWith("#TASK"))
+    {
+      // #TASK <id>|<hour>|<minute>|<repeat>|<amount>|<enabled>
+      schedule_task_t task = scheduler.parseTask(message.substring(6));
+      if (task.id == 0)
+      {
+        webSocket.sendTXT(num, "Invalid task format");
+        break;
+      }
+      bool result = scheduler.addTask(task);
+      if (!result)
+      {
+        webSocket.sendTXT(num, "Failed to add task");
+        break;
+      }
+      uint8_t* ret = (uint8_t*)("Task added " + String(task.id)).c_str();
+      webSocket.sendTXT(num, ret, sizeof(ret));
+    }
+    break;
+  }
+}
+
+void feed(uint8_t amount, std::function<void()> callback)
+{
+  feeder.feed(amount, callback);
+  feedLog.add(amount);
+}
+
+void mainLoop()
+{
+  scheduler.run();
 }
